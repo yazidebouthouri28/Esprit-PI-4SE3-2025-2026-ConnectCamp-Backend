62fc52fba5229bfc98013036d94ffd589148523a
yazidebouthouri28 <ybouthouri470@gmail.com>
2026-05-05 02:49:14 +0100

    CORS Config for Azurzzzzzzze

--- src/main/java/tn/esprit/projetintegre/config/SecurityConfig.java
+++ src/main/java/tn/esprit/projetintegre/config/SecurityConfig.java
@@ -47,6 +47,7 @@ public class SecurityConfig {
                 }))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(org.springframework.http.HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers(
                                 "/",
                                 "/auth/**",
